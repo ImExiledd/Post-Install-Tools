105e6c1aab6e9ed925ce04ab9dd5557d2b027d34
Exile <csxkingjp@gmail.com>
2020-12-23 11:38:17 -0700

    Added user interface and basic extra feature functionality

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,237 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.installBtn = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.wipBox = New System.Windows.Forms.CheckBox()
+        Me.confirmBox = New System.Windows.Forms.CheckBox()
+        Me.modsPanel = New System.Windows.Forms.Panel()
+        Me.CheckBox9 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.modsBtn = New System.Windows.Forms.Button()
+        Me.logBtn = New System.Windows.Forms.Button()
+        Me.modsPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'installBtn
+        '
+        Me.installBtn.Location = New System.Drawing.Point(12, 362)
+        Me.installBtn.Name = "installBtn"
+        Me.installBtn.Size = New System.Drawing.Size(448, 59)
+        Me.installBtn.TabIndex = 0
+        Me.installBtn.Text = "Install Applications (X)"
+        Me.installBtn.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(99, 29)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Discord"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.ForeColor = System.Drawing.Color.White
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 47)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(87, 29)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Steam"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.ForeColor = System.Drawing.Color.White
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 82)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(87, 29)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Origin"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.ForeColor = System.Drawing.Color.White
+        Me.CheckBox4.Location = New System.Drawing.Point(12, 117)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(182, 29)
+        Me.CheckBox4.TabIndex = 4
+        Me.CheckBox4.Text = "Windows Terminal"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.ForeColor = System.Drawing.Color.White
+        Me.CheckBox5.Location = New System.Drawing.Point(12, 152)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(115, 29)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "Opera GX"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.ForeColor = System.Drawing.Color.White
+        Me.CheckBox6.Location = New System.Drawing.Point(12, 187)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(164, 29)
+        Me.CheckBox6.TabIndex = 6
+        Me.CheckBox6.Text = "Google Chrome"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.ForeColor = System.Drawing.Color.White
+        Me.CheckBox7.Location = New System.Drawing.Point(12, 222)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(152, 29)
+        Me.CheckBox7.TabIndex = 7
+        Me.CheckBox7.Text = "Mozilla Firefox"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.ForeColor = System.Drawing.Color.White
+        Me.CheckBox8.Location = New System.Drawing.Point(12, 257)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(175, 29)
+        Me.CheckBox8.TabIndex = 8
+        Me.CheckBox8.Text = "Epic Games Store"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'wipBox
+        '
+        Me.wipBox.AutoSize = True
+        Me.wipBox.ForeColor = System.Drawing.Color.White
+        Me.wipBox.Location = New System.Drawing.Point(12, 292)
+        Me.wipBox.Name = "wipBox"
+        Me.wipBox.Size = New System.Drawing.Size(70, 29)
+        Me.wipBox.TabIndex = 9
+        Me.wipBox.Text = "WIP"
+        Me.wipBox.UseVisualStyleBackColor = True
+        '
+        'confirmBox
+        '
+        Me.confirmBox.AutoSize = True
+        Me.confirmBox.ForeColor = System.Drawing.Color.White
+        Me.confirmBox.Location = New System.Drawing.Point(12, 327)
+        Me.confirmBox.Name = "confirmBox"
+        Me.confirmBox.Size = New System.Drawing.Size(171, 29)
+        Me.confirmBox.TabIndex = 10
+        Me.confirmBox.Text = "Confirm Options"
+        Me.confirmBox.UseVisualStyleBackColor = True
+        '
+        'modsPanel
+        '
+        Me.modsPanel.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.modsPanel.Controls.Add(Me.CheckBox9)
+        Me.modsPanel.Controls.Add(Me.Label1)
+        Me.modsPanel.Location = New System.Drawing.Point(200, 12)
+        Me.modsPanel.Name = "modsPanel"
+        Me.modsPanel.Size = New System.Drawing.Size(557, 344)
+        Me.modsPanel.TabIndex = 11
+        '
+        'CheckBox9
+        '
+        Me.CheckBox9.AutoSize = True
+        Me.CheckBox9.Location = New System.Drawing.Point(16, 61)
+        Me.CheckBox9.Name = "CheckBox9"
+        Me.CheckBox9.Size = New System.Drawing.Size(145, 29)
+        Me.CheckBox9.TabIndex = 1
+        Me.CheckBox9.Text = "BetterDiscord"
+        Me.CheckBox9.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Discord Modifications"
+        '
+        'modsBtn
+        '
+        Me.modsBtn.Location = New System.Drawing.Point(466, 362)
+        Me.modsBtn.Name = "modsBtn"
+        Me.modsBtn.Size = New System.Drawing.Size(192, 59)
+        Me.modsBtn.TabIndex = 12
+        Me.modsBtn.Text = "Show Modifications Panel"
+        Me.modsBtn.UseVisualStyleBackColor = True
+        '
+        'logBtn
+        '
+        Me.logBtn.Location = New System.Drawing.Point(664, 362)
+        Me.logBtn.Name = "logBtn"
+        Me.logBtn.Size = New System.Drawing.Size(93, 59)
+        Me.logBtn.TabIndex = 0
+        Me.logBtn.Text = "Log"
+        Me.logBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(769, 433)
+        Me.Controls.Add(Me.logBtn)
+        Me.Controls.Add(Me.modsBtn)
+        Me.Controls.Add(Me.modsPanel)
+        Me.Controls.Add(Me.confirmBox)
+        Me.Controls.Add(Me.wipBox)
+        Me.Controls.Add(Me.CheckBox8)
+        Me.Controls.Add(Me.CheckBox7)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.installBtn)
+        Me.Name = "Form1"
+        Me.Text = "Exile's Post Install Tools"
+        Me.modsPanel.ResumeLayout(False)
+        Me.modsPanel.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents installBtn As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents wipBox As CheckBox
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents modsPanel As Panel
+    Friend WithEvents modsBtn As Button
+    Friend WithEvents logBtn As Button
+    Friend WithEvents confirmBox As CheckBox
+    Friend WithEvents CheckBox9 As CheckBox
+    Friend WithEvents Label1 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Dim modsPanelShown As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        modsPanel.Hide()
+        logBtn.Enabled = False
+        installBtn.Enabled = False
+        wipBox.Enabled = False
+    End Sub
 
+    Private Sub confirmBox_CheckedChanged(sender As Object, e As EventArgs) Handles confirmBox.CheckedChanged
+        If confirmBox.Checked = True Then
+            installBtn.Enabled = True
+        Else
+            installBtn.Enabled = False
+        End If
+    End Sub
+
+    Private Sub modsBtn_Click(sender As Object, e As EventArgs) Handles modsBtn.Click
+        If modsPanelShown = False Then
+            modsPanel.Show()
+            modsBtn.Text = "Hide Modifications Panel"
+            modsPanelShown = True
+        ElseIf modsPanelShown = True Then
+            modsPanel.Hide()
+            modsBtn.Text = "Show Modifications Panel"
+            modsPanelShown = False
+        End If
+    End Sub
 End Class
